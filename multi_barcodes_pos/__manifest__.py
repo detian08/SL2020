1cbfacb16770f2bf61039aaa22940fb76f6de1a5
Mahmoud El shorbagy <ma7moud.elshorbagy@gmail.com>
2020-08-04 22:23:53 +0200

    multi_barcode_pos

---
+++ multi_barcodes_pos/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'POS Product Multi Barcode',
+    'summary': """Allows to create multiple barcode for a single product""",
+    'description': """Allows to create multiple barcode for a single product""",
+    'author': 'M.Shorbagy',
+    'category': 'Point of Sale',
+    'depends': ['product', 'point_of_sale'],
+    'license': 'AGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_views.xml',
+        'views/pos_template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
